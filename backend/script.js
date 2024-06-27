8423e3783e500d40f8cf8cda73a8388f972dc15f
Salil1212 <salilnigam1212@gmai.com>
2024-06-28 02:37:26 +0530

    package.json updated

--- backend/script.js
+++ backend/script.js
@@ -5,11 +5,7 @@ const bodyParser = require("body-parser");
 const taskRoutes = require("./routes/tasks");
 require("dotenv").config();
 const app = express();
-
-// mongoose.connect("mongodb://localhost:27017/taskdb", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+const port = process.env.PORT || 6000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,16 +14,10 @@ app.use(express.json());
 app.use("/api/tasks", taskRoutes);
 app.get("/home", (req, res) => res.json("This is home page"));
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
 
-// mongoose.connect(process.env.mongoDBURL).then(() => {
-//   console.log("App connected to database");
-// });
-// app.listen(process.env.PORT, () => {
-//   console.log(`App is listening to port: ${process.env.PORT}`);
-// });
 async function main() {
   await mongoose.connect(process.env.mongoDBURL);
   console.log("db connected");
@@ -35,17 +25,3 @@ async function main() {
 main().catch((err) => {
   console.log(err);
 });
-// {
-//   "version": 2,
-//   "builds": [
-//     {
-//       "src": "backend/script.js",
-//       "use": "@vercel/node"
-//     }
-//   ],
-//   "routes": [
-//     {
-//       "src": "/api/(.*)",
-//       "dest": "backend/script.js"
-//     }  ]
-// }
