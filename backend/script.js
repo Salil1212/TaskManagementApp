94e3382ce17ad5cb2319f167837cad2a909aa059
Salil1212 <salilnigam1212@gmai.com>
2024-06-28 09:42:06 +0530

    No changes

--- backend/script.js
+++ backend/script.js
@@ -11,6 +11,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  console.log("Api is running");
+});
 app.use("/api/tasks", taskRoutes);
 app.get("/home", (req, res) => res.json("This is home page"));
 
